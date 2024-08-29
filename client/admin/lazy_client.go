a9837c84657f99660343d55a4fc01550ae170842
Haifeng He <haifeng.he@temporal.io>
2024-08-29 15:16:18 -0700

    Add Lazy temporal client

---
+++ client/admin/lazy_client.go
@@ -0,0 +1,36 @@
+package admin
+
+import (
+	"context"
+
+	"github.com/temporalio/s2s-proxy/client"
+	"go.temporal.io/server/api/adminservice/v1"
+	"google.golang.org/grpc"
+)
+
+type (
+	lazyClient struct {
+		clientProvider client.ClientProvider
+	}
+)
+
+func NewLazyClient(
+	clientProvider client.ClientProvider,
+) adminservice.AdminServiceClient {
+	return &lazyClient{
+		clientProvider: clientProvider,
+	}
+}
+
+func (c *lazyClient) StreamWorkflowReplicationMessages(
+	ctx context.Context,
+	opts ...grpc.CallOption,
+) (adminservice.AdminService_StreamWorkflowReplicationMessagesClient, error) {
+	var resp adminservice.AdminService_StreamWorkflowReplicationMessagesClient
+	client, err := c.clientProvider.GetAdminClient()
+	if err != nil {
+		return resp, err
+	}
+
+	return client.StreamWorkflowReplicationMessages(ctx, opts...)
+}
