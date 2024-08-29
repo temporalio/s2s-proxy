f774c1217af465ad6d789cd5a237d2767f2566a1
Paul Glass <pnglass@gmail.com>
2024-08-29 15:56:35 -0500

    Unit test namespace name translation of GetNamespaceReplicationMessagesResponse (#24)
    * Unit test translation of GetNamespaceReplicationMessagesResponse
    
    * Rename fn

--- interceptor/namespace_translator_test.go
+++ interceptor/namespace_translator_test.go
@@ -6,7 +6,11 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.temporal.io/api/command/v1"
 	"go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/namespace/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/api/adminservice/v1"
+	enumsspb "go.temporal.io/server/api/enums/v1"
+	replicationspb "go.temporal.io/server/api/replication/v1"
 )
 
 func TestSetNamespaceBasedOnCluster(t *testing.T) {
@@ -29,7 +33,6 @@ func TestSetNamespaceBasedOnCluster(t *testing.T) {
 			Other  string
 			Nested []*StructWithNamespaceField
 		}
-
 		StructWithCircularPointer struct {
 			Link      *StructWithCircularPointer
 			Namespace string
@@ -188,6 +191,169 @@ func TestSetNamespaceBasedOnCluster(t *testing.T) {
 					}
 				})
 			}
+		})
+	}
+}
+
+func TestTranslateNamespaceReplicationMessages(t *testing.T) {
+	makeFullType := func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+		return &adminservice.GetNamespaceReplicationMessagesResponse{
+			Messages: &replicationspb.ReplicationMessages{
+				ReplicationTasks: []*replicationspb.ReplicationTask{
+					{
+						TaskType:     enumsspb.REPLICATION_TASK_TYPE_NAMESPACE_TASK,
+						SourceTaskId: 123,
+						Attributes: &replicationspb.ReplicationTask_NamespaceTaskAttributes{
+							NamespaceTaskAttributes: &replicationspb.NamespaceTaskAttributes{
+								NamespaceOperation: 345,
+								Id:                 "abc",
+								Info: &namespace.NamespaceInfo{
+									Name:        ns,
+									State:       789,
+									Description: "do not change",
+									OwnerEmail:  "not this either",
+									Data:        map[string]string{"some": "val"},
+									Id:          "",
+									Capabilities: &namespace.NamespaceInfo_Capabilities{
+										EagerWorkflowStart: true,
+										SyncUpdate:         true,
+										AsyncUpdate:        true,
+									},
+									SupportsSchedules: true,
+								},
+								ConfigVersion:   2,
+								FailoverVersion: 3,
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	cases := []struct {
+		testName   string
+		mapping    map[string]string
+		expChanged bool
+		makeType   func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse
+	}{
+		{
+			testName: "nil",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return nil
+			},
+		},
+		{
+			testName: "nil messages",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{}
+			},
+		},
+		{
+			testName: "nil replication tasks",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{
+					Messages: &replicationspb.ReplicationMessages{
+						ReplicationTasks: nil,
+					},
+				}
+			},
+		},
+		{
+			testName: "nil replication tasks item",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{
+					Messages: &replicationspb.ReplicationMessages{
+						ReplicationTasks: []*replicationspb.ReplicationTask{
+							nil,
+						},
+						LastRetrievedMessageId: 1234,
+					},
+				}
+			},
+		},
+		{
+			testName: "nil attributes",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{
+					Messages: &replicationspb.ReplicationMessages{
+						ReplicationTasks: []*replicationspb.ReplicationTask{
+							{},
+						},
+						LastRetrievedMessageId: 1234,
+					},
+				}
+			},
+		},
+		{
+			testName: "nil namespace task attributes",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{
+					Messages: &replicationspb.ReplicationMessages{
+						ReplicationTasks: []*replicationspb.ReplicationTask{
+							{
+								TaskType:     enumsspb.REPLICATION_TASK_TYPE_NAMESPACE_TASK,
+								SourceTaskId: 123,
+								Attributes: &replicationspb.ReplicationTask_NamespaceTaskAttributes{
+									NamespaceTaskAttributes: nil,
+								},
+							},
+						},
+						LastRetrievedMessageId: 1234,
+					},
+				}
+			},
+		},
+		{
+			testName: "nil namespace info",
+			makeType: func(ns string) *adminservice.GetNamespaceReplicationMessagesResponse {
+				return &adminservice.GetNamespaceReplicationMessagesResponse{
+					Messages: &replicationspb.ReplicationMessages{
+						ReplicationTasks: []*replicationspb.ReplicationTask{
+							{
+								TaskType:     enumsspb.REPLICATION_TASK_TYPE_NAMESPACE_TASK,
+								SourceTaskId: 123,
+								Attributes: &replicationspb.ReplicationTask_NamespaceTaskAttributes{
+									NamespaceTaskAttributes: &replicationspb.NamespaceTaskAttributes{
+										Info: nil,
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+		},
+		{
+			testName: "nil mapping",
+			makeType: makeFullType,
+		},
+		{
+			testName:   "unmapped namespace unchanged",
+			mapping:    map[string]string{"other-ns": "other-ns.cloud"},
+			expChanged: false,
+			makeType:   makeFullType,
+		},
+		{
+			testName:   "matched namespace changed",
+			mapping:    map[string]string{"orig": "orig.cloud"},
+			expChanged: true,
+			makeType:   makeFullType,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.testName, func(t *testing.T) {
+			input := c.makeType("orig")
+			var expOutput *adminservice.GetNamespaceReplicationMessagesResponse
+			if c.expChanged {
+				expOutput = c.makeType("orig.cloud")
+			} else {
+				expOutput = c.makeType("orig")
+			}
+			changed := translate_GetNamespaceReplicationMessagesResponse(input, c.mapping)
+			require.Equal(t, c.expChanged, changed)
+			require.Equal(t, expOutput, input)
 		})
 	}
 }
