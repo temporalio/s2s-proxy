342b8e3520cd9a8d1df6b1e04fa5338ccf900d4c
Haifeng He <haifeng.he@temporal.io>
2024-08-29 16:11:12 -0700

    Add develop readme

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -6,5 +6,3 @@ openssl req -x509 -newkey rsa:4096 -keyout develop/certificates/proxy1.key -out 
 
 echo "Generating Cluster Certificate for onebox-proxy2.cluster.tmprl.cloud"
 openssl req -x509 -newkey rsa:4096 -keyout develop/certificates/proxy2.key -out develop/certificates/proxy2.pem -days 365 -nodes -addext "subjectAltName = DNS:onebox-proxy2.cluster.tmprl.cloud" -subj "/C=US/ST=WA/O=Temporal/CN=onebox-proxy2.cluster.tmprl.cloud"
-
-echo "Added yml files to develop/secrets"
