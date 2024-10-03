68f6a6153cfff2c18a6e0e21238c2f368d9580e6
Paul Glass <pnglass@gmail.com>
2024-10-03 12:29:16 -0500

    Remove reflectionRecursionMaxDepth (#36)

--- interceptor/namespace_translator.go
+++ interceptor/namespace_translator.go
@@ -2,24 +2,13 @@ package interceptor
 
 import (
 	"context"
-	"fmt"
-	"reflect"
 	"strings"
 
 	"github.com/temporalio/s2s-proxy/config"
-	"go.temporal.io/server/api/adminservice/v1"
-	replicationspb "go.temporal.io/server/api/replication/v1"
 	"go.temporal.io/server/common/api"
 	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/log/tag"
 	"google.golang.org/grpc"
-)
-
-const (
-	// Currently, name translation relies on reflection to recursively find namespace name fields
-	// in request and response objects. This is the default max depth allowed for that recursion
-	// to protect against circular pointers or etc. It is configurable.
-	defaultReflectionRecursionMaxDepth int = 100
 )
 
 type (
@@ -27,7 +16,6 @@ type (
 		logger                      log.Logger
 		requestNameMapping          map[string]string
 		responseNameMapping         map[string]string
-		reflectionRecursionMaxDepth int
 	}
 )
 
@@ -58,7 +46,6 @@ func NewNamespaceNameTranslator(
 		logger:                      logger,
 		requestNameMapping:          requestNameMapping,
 		responseNameMapping:         responseNameMapping,
-		reflectionRecursionMaxDepth: cfg.NamespaceNameTranslation.ReflectionRecursionMaxDepth,
 	}
 }
 
@@ -74,141 +61,25 @@ func (i *NamespaceNameTranslator) Intercept(
 		return handler(ctx, req)
 	}
 
-	methodName := api.MethodName(info.FullMethod)
-	if strings.HasPrefix(info.FullMethod, api.WorkflowServicePrefix) {
-		i.logger.Debug("intercepted workflowservice request", tag.NewStringTag("method", methodName))
+	if strings.HasPrefix(info.FullMethod, api.WorkflowServicePrefix) ||
+		strings.HasPrefix(info.FullMethod, api.AdminServicePrefix) {
+
+		methodName := api.MethodName(info.FullMethod)
+		i.logger.Debug("intercepted request", tag.NewStringTag("method", methodName))
 
 		// Translate namespace name in request.
-		changed, trErr := translateNamespace(req, i.requestNameMapping, i.reflectionRecursionMaxDepth)
+		changed, trErr := translateNamespace(req, i.requestNameMapping)
 		logTranslateNamespaceResult(i.logger, changed, trErr, methodName+"Request", req)
 
 		resp, err := handler(ctx, req)
 
 		// Translate namespace name in response.
-		changed, trErr = translateNamespace(resp, i.responseNameMapping, i.reflectionRecursionMaxDepth)
+		changed, trErr = translateNamespace(resp, i.responseNameMapping)
 		logTranslateNamespaceResult(i.logger, changed, trErr, methodName+"Response", resp)
-		return resp, err
-	} else if strings.HasPrefix(info.FullMethod, api.AdminServicePrefix) {
-		i.logger.Debug("intercepted adminservice request", tag.NewStringTag("method", methodName))
-
-		// Translate namespace name in request.
-		changed, trErr := translateNamespace(req, i.requestNameMapping, i.reflectionRecursionMaxDepth)
-		logTranslateNamespaceResult(i.logger, changed, trErr, methodName+"Request", req)
-
-		resp, err := handler(ctx, req)
-
-		switch rt := resp.(type) {
-		case *adminservice.GetNamespaceReplicationMessagesResponse:
-			// Need special handling for the `Info.Name` field.
-			changed := translate_GetNamespaceReplicationMessagesResponse(rt, i.responseNameMapping)
-			logTranslateNamespaceResult(i.logger, changed, nil, methodName+"Response", resp)
-		}
-
-		changed, trErr = translateNamespace(resp, i.responseNameMapping, i.reflectionRecursionMaxDepth)
-		logTranslateNamespaceResult(i.logger, changed, trErr, methodName+"Response", resp)
-
 		return resp, err
 	} else {
 		return handler(ctx, req)
 	}
-}
-
-func translate_GetNamespaceReplicationMessagesResponse(rt *adminservice.GetNamespaceReplicationMessagesResponse, mapping map[string]string) bool {
-	var changed bool
-	if rt == nil || rt.Messages == nil {
-		return changed
-	}
-
-	for _, task := range rt.Messages.ReplicationTasks {
-		if task == nil || task.Attributes == nil {
-			continue
-		}
-		switch attr := task.Attributes.(type) {
-		case *replicationspb.ReplicationTask_NamespaceTaskAttributes:
-			if attr == nil || attr.NamespaceTaskAttributes == nil || attr.NamespaceTaskAttributes.Info == nil {
-				continue
-			}
-			oldName := attr.NamespaceTaskAttributes.Info.Name
-			newName, found := mapping[oldName]
-			if found {
-				attr.NamespaceTaskAttributes.Info.Name = newName
-				changed = true
-			}
-		}
-	}
-	return changed
-}
-
-func translateNamespace(obj any, mapping map[string]string, maxDepth int) (bool, error) {
-	if maxDepth <= 0 {
-		maxDepth = defaultReflectionRecursionMaxDepth
-	}
-	val := reflect.ValueOf(obj)
-	return translateNamespaceRecursive(val, mapping, 0, maxDepth)
-}
-
-func translateNamespaceRecursive(val reflect.Value, mapping map[string]string, depth, maxDepth int) (bool, error) {
-	if depth > maxDepth {
-		// Protect against potential circular pointer.
-		return false, fmt.Errorf("translateNamespaceRecursive max depth reached")
-	}
-
-	var changed bool
-
-	switch val.Kind() {
-	case reflect.Ptr, reflect.Interface:
-		c, err := translateNamespaceRecursive(val.Elem(), mapping, depth+1, maxDepth)
-		changed = changed || c
-		if err != nil {
-			return changed, err
-		}
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-
-			fieldType := val.Type().Field(i)
-			if !fieldType.IsExported() {
-				continue
-			}
-
-			if field.Kind() != reflect.String {
-				c, err := translateNamespaceRecursive(field, mapping, depth+1, maxDepth)
-				changed = changed || c
-				if err != nil {
-					return changed, err
-				}
-			} else {
-				for _, nsFieldName := range []string{
-					"Namespace",
-					"WorkflowNamespace", // PollActivityTaskQueueResponse
-				} {
-					if fieldType.Name != nsFieldName {
-						continue
-					}
-
-					old := field.String()
-					new, ok := mapping[old]
-					if !ok {
-						continue
-					}
-					field.SetString(new)
-					if old != new {
-						changed = true
-					}
-				}
-			}
-		}
-	case reflect.Slice, reflect.Array:
-		for i := 0; i < val.Len(); i++ {
-			el := val.Index(i)
-			c, err := translateNamespaceRecursive(el, mapping, depth+1, maxDepth)
-			changed = changed || c
-			if err != nil {
-				return changed, err
-			}
-		}
-	}
-	return changed, nil
 }
 
 func logTranslateNamespaceResult(logger log.Logger, changed bool, err error, methodName string, obj any) {
