11b89b4967f53e634db37a4553905c6608f82d86
Paul Glass <paul.glass@temporal.io>
2025-06-18 17:15:06 -0500

    address comments

--- interceptor/translator.go
+++ interceptor/translator.go
@@ -1,11 +1,5 @@
 package interceptor
 
-import (
-	"strings"
-
-	"go.temporal.io/server/common/api"
-)
-
 type (
 	Translator interface {
 		MatchMethod(string) bool
@@ -18,12 +12,6 @@ type (
 		matchReq    stringMatcher
 		matchResp   stringMatcher
 		visitor     visitor
-	}
-
-	saTranslator struct {
-		matchMethod func(string) bool
-		reqMap      map[string]map[string]string
-		respMap     map[string]map[string]string
 	}
 )
 
@@ -46,46 +34,6 @@ func (n *translatorImpl) TranslateRequest(req any) (bool, error) {
 
 func (n *translatorImpl) TranslateResponse(resp any) (bool, error) {
 	return n.visitor(resp, n.matchResp)
-}
-
-func NewSearchAttributeTranslator(reqMap, respMap map[string]map[string]string) Translator {
-	return &saTranslator{
-		matchMethod: func(method string) bool {
-			// In workflowservice APIs, responses only contain the search attribute alias.
-			// We should never translate these responses to the search attribute's indexed field.
-			return !strings.HasPrefix(method, api.WorkflowServicePrefix)
-		},
-		reqMap:  reqMap,
-		respMap: respMap,
-	}
-}
-
-func (s *saTranslator) MatchMethod(m string) bool {
-	return s.matchMethod(m)
-}
-
-func (s *saTranslator) TranslateRequest(req any) (bool, error) {
-	return visitSearchAttributes(req, s.getNamespaceReqMatcher(""))
-}
-
-func (s *saTranslator) TranslateResponse(resp any) (bool, error) {
-	return visitSearchAttributes(resp, s.getNamespaceRespMatcher(""))
-}
-
-func (s *saTranslator) getNamespaceReqMatcher(namespaceId string) stringMatcher {
-	// Placeholder: Just return the first one (only support one namespace mapping)
-	for _, m := range s.reqMap {
-		return createStringMatcher(m)
-	}
-	return createStringMatcher(nil)
-}
-
-func (s *saTranslator) getNamespaceRespMatcher(namespaceId string) stringMatcher {
-	// Placeholder: Just return the first one (only support one namespace mappping)
-	for _, m := range s.respMap {
-		return createStringMatcher(m)
-	}
-	return createStringMatcher(nil)
 }
 
 func createStringMatcher(mapping map[string]string) stringMatcher {
