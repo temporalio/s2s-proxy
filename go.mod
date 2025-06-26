82231828f174b03c3b6fb97886f2092f67c26c17
Paul Glass <paul.glass@temporal.io>
2025-06-25 20:15:11 -0500

    Vendor in gogo-based serializer from v1.22.2

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/temporalio/s2s-proxy
 go 1.23.4
 
 require (
+	github.com/gogo/protobuf v1.3.2
 	github.com/gogo/status v1.1.1
 	github.com/golang/mock v1.7.0-rc.1
 	github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus v1.1.0
@@ -12,10 +13,15 @@ require (
 	github.com/stretchr/testify v1.10.0
 	github.com/urfave/cli/v2 v2.27.2
 	go.temporal.io/api v1.43.0
+	go.temporal.io/sdk v1.31.0
 	go.temporal.io/server v1.26.2
+	go.uber.org/atomic v1.11.0
 	go.uber.org/fx v1.22.0
 	go.uber.org/mock v0.4.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/exp v0.0.0-20240531132922-fd00a4e0eefc
 	golang.org/x/text v0.23.0
+	golang.org/x/time v0.5.0
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/yaml.v3 v3.0.1
@@ -33,7 +39,6 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/googleapis v0.0.0-20180223154316-0cd9801be74a // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
@@ -62,16 +67,11 @@ require (
 	go.opentelemetry.io/otel/sdk v1.31.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.31.0 // indirect
 	go.opentelemetry.io/otel/trace v1.31.0 // indirect
-	go.temporal.io/sdk v1.31.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/dig v1.17.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/exp v0.0.0-20240531132922-fd00a4e0eefc // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241007155032-5fefd90f89a9 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241007155032-5fefd90f89a9 // indirect
 )
---
+++ vendor-protos.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -x
+
+TARGET_PKG="common/proto122"
+rm -rf "./$TARGET_PKG"
+mkdir -p "$TARGET_PKG/api" "$TARGET_PKG/server"
+
+COMMON_DIR=$(realpath ./common)
+TARGET_DIR=$(realpath "./$TARGET_PKG")
+
+API_DIR="${COMMON_DIR}/.tmp-api"
+SERVER_DIR="${COMMON_DIR}/.tmp-server"
+if [ ! -d "$API_DIR" ]; then
+    git clone https://github.com/temporalio/api-go.git "$API_DIR"
+fi
+(
+    cd "$API_DIR"
+    git reset --hard bb03061759c82712a4b933f5175834baebee9c9a
+)
+
+if [ ! -d "$SERVER_DIR" ]; then
+    git clone https://github.com/temporalio/temporal.git "$SERVER_DIR"
+fi
+(
+    cd "$SERVER_DIR"
+    git fetch --tags
+    git reset --hard v1.22.2
+)
+
+for pkg in \
+    common \
+    enums \
+    errordetails \
+    failure \
+    history \
+    internal/temporaljsonpb \
+    namespace \
+    replication \
+    sdk \
+    serviceerror \
+    taskqueue \
+    update \
+    version \
+    workflow \
+; do
+    SRC="$API_DIR/${pkg}"
+    DEST="${TARGET_DIR}/api/${pkg}"
+    mkdir -p "$(dirname $DEST)"
+    cp -R "$SRC" "$DEST"
+
+    # Fix import paths
+    find "$DEST" -type f | \
+        xargs -n 1 sed -i '' "s;go.temporal.io/api;github.com/temporalio/s2s-proxy/${TARGET_PKG}/api;g"
+done
+
+for pkg in \
+    api/persistence \
+    api/clock \
+    api/enums \
+    api/history \
+    api/namespace \
+    api/replication \
+    api/taskqueue \
+    api/update \
+    api/workflow \
+    common/constants.go \
+    common/debug/debug.go \
+    common/util \
+    common/persistence/serialization \
+    common/definition \
+    common/primitives/timestamp \
+    common/codec/jsonpb.go \
+    common/tasks \
+    common/log \
+    common/quotas \
+    common/primitives/role.go \
+    common/backoff/retrypolicy.go \
+    service/history/tasks \
+; do
+    SRC="$SERVER_DIR/${pkg}"
+    DEST="${TARGET_DIR}/server/${pkg}"
+    mkdir -p "$(dirname $DEST)"
+    cp -R "$SRC" "$DEST"
+
+    # Fix import paths
+    find "$DEST" -type f | \
+        xargs -n 1 sed -i '' "s;go.temporal.io/server;github.com/temporalio/s2s-proxy/${TARGET_PKG}/server;g"
+    find "$DEST" -type f | \
+        xargs -n 1 sed -i '' "s;go.temporal.io/api;github.com/temporalio/s2s-proxy/${TARGET_PKG}/api;g"
+
+    # Remove '//go:build TEMPORAL_DEBUG' build constraint
+    # ???
+    find "$DEST" -type f | \
+        xargs -n 1 sed -i '' "s;//.*TEMPORAL_DEBUG;;g"
+
+    # Unnecessary files
+    rm -f "${TARGET_DIR}/server/service/history/tasks/predicates.go"
+    rm -f "${TARGET_DIR}/server/common/quotas/delayed_request_rate_limiter.go"
+    rm -f "${TARGET_DIR}/server/common/quotas/delayed_request_rate_limiter_test.go"
+    rm -f "${TARGET_DIR}/server/common/tasks/fifo_scheduler.go"
+    rm -f "${TARGET_DIR}/server/common/tasks/interleaved_weighted_round_robin.go"
+    rm -f "${TARGET_DIR}/server/common/tasks/sequential_scheduler.go"
+
+    find "$TARGET_DIR" -type f -name '*test.go' -delete
+done
+
+go mod tidy
+make fmt
+
+rm -rf "$API_DIR"
+rm -rf "$SERVER_DIR"
