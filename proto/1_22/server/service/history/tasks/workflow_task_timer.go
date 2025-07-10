72a0dc352c2a1df7c03ac2fe9651b01c3a541326
Paul Glass <pnglass@gmail.com>
2025-07-10 14:54:11 -0500

    Vendor gogo-based protos and serializer from Temporal v1.22.2 (#117)

---
+++ proto/1_22/server/service/history/tasks/workflow_task_timer.go
@@ -0,0 +1,95 @@
+// Copied from https://github.com/temporalio/temporal/tree/v1.22.2. DO NOT EDIT.
+
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package tasks
+
+import (
+	"sync/atomic"
+	"time"
+
+	enumspb "github.com/temporalio/s2s-proxy/proto/1_22/api/enums/v1"
+
+	enumsspb "github.com/temporalio/s2s-proxy/proto/1_22/server/api/enums/v1"
+	"github.com/temporalio/s2s-proxy/proto/1_22/server/common/definition"
+)
+
+var _ Task = (*WorkflowTaskTimeoutTask)(nil)
+
+type (
+	WorkflowTaskTimeoutTask struct {
+		definition.WorkflowKey
+		VisibilityTimestamp time.Time
+		TaskID              int64
+		EventID             int64
+		ScheduleAttempt     int32
+		TimeoutType         enumspb.TimeoutType
+		Version             int64
+
+		// state is used by speculative WT only.
+		state atomic.Uint32 // of type ctasks.State
+	}
+)
+
+func (d *WorkflowTaskTimeoutTask) GetKey() Key {
+	return NewKey(d.VisibilityTimestamp, d.TaskID)
+}
+
+func (d *WorkflowTaskTimeoutTask) GetVersion() int64 {
+	return d.Version
+}
+
+func (d *WorkflowTaskTimeoutTask) SetVersion(version int64) {
+	d.Version = version
+}
+
+func (d *WorkflowTaskTimeoutTask) GetTaskID() int64 {
+	return d.TaskID
+}
+
+func (d *WorkflowTaskTimeoutTask) SetTaskID(id int64) {
+	d.TaskID = id
+}
+
+func (d *WorkflowTaskTimeoutTask) GetVisibilityTime() time.Time {
+	return d.VisibilityTimestamp
+}
+
+func (d *WorkflowTaskTimeoutTask) SetVisibilityTime(t time.Time) {
+	d.VisibilityTimestamp = t
+}
+
+func (d *WorkflowTaskTimeoutTask) GetCategory() Category {
+	return CategoryTimer
+}
+
+func (d *WorkflowTaskTimeoutTask) GetType() enumsspb.TaskType {
+	return enumsspb.TASK_TYPE_WORKFLOW_TASK_TIMEOUT
+}
+
+// Cancel and State are used by in-memory WorkflowTaskTimeoutTask (for speculative WT) only.
+// TODO (alex): They need to be moved to speculativeWorkflowTaskTimeoutExecutable
+// and workflowTaskStateMachine should somehow signal that executable directly.
+// Major refactoring needs to be done to achieve that.
