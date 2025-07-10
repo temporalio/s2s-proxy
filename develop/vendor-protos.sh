45deefeed6440f5fe627d8f8928342cde592948a
Paul Glass <paul.glass@temporal.io>
2025-07-10 12:12:26 -0500

    Readme improvement

--- develop/vendor-protos.sh
+++ develop/vendor-protos.sh
@@ -65,6 +65,12 @@ write_readme() {
 # Overview
 
 This is a partial copy of [${repo} at ${commit}](${tree_link})
+
+Temporal v1.22 used gogo-protobuf for serialization, which allowed it to store
+invalid UTF8 provided from the SDK and workers locally. Temporal 1.23 switched
+to google-protobuf, which requires all string data be valid UTF8. The proxy uses
+these proto definitions to seamlessly repair broken UTF8 strings in flight
+without dropping any of the valid data.
 EOF
 }
 
