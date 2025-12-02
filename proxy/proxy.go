a0518c15275f4404c2d789c4fe2c53cc2e73d666
Nick Beaumont <nicholas.beaumont@temporal.io>
2025-12-02 10:52:46 -0800

    Remove broken health checks for now (#176)
    * Remove broken health checks for now
    
    * Remove unused code

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -125,12 +125,7 @@ func (s *Proxy) Start() error {
 	if s.inboundHealthCheckConfig != nil {
 		var err error
 		healthFn := func() bool {
-			// TODO: overly conservative right now. When there are multiple remotes, we should track their health separately
-			for _, cc := range s.clusterConnections {
-				if !cc.AcceptingInboundTraffic() {
-					return false
-				}
-			}
+			// TODO: Rethink health checks. The inbound/outbound traffic availability isn't quite right for a health check
 			return true
 		}
 		if s.inboundHealthCheckServer, err = s.startHealthCheckHandler(s.lifetime, newInboundHealthCheck(healthFn, s.logger), *s.inboundHealthCheckConfig); err != nil {
@@ -143,12 +138,7 @@ func (s *Proxy) Start() error {
 
 	if s.outboundHealthCheckConfig != nil {
 		healthFn := func() bool {
-			// TODO: overly conservative right now. When there are multiple remotes, we should track their health separately
-			for _, cc := range s.clusterConnections {
-				if !cc.AcceptingOutboundTraffic() {
-					return false
-				}
-			}
+			// TODO: Rethink health checks. The inbound/outbound traffic availability isn't quite right for a health check
 			return true
 		}
 		var err error
