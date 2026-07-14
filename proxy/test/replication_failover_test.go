19826458229bd1c62acb54432c424a3e29a4ac18
Haifeng He <haifeng.he@temporal.io>
2026-07-14 09:45:03 -0700

    upgrade go.temporal.io/server v1.32.0-157.6

--- proxy/test/replication_failover_test.go
+++ proxy/test/replication_failover_test.go
@@ -780,7 +780,7 @@ func (s *ReplicationTestSuite) failoverNamespace(
 				if err != nil || resp == nil {
 					return false
 				}
-				if resp.ActiveClusterName() != targetCluster {
+				if resp.ActiveClusterName(namespace.RoutingKey{}) != targetCluster {
 					return false
 				}
 			}
